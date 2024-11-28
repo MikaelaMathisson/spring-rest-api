bf40d2b2b49b5b1ce0585acaf24c547c10092631
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-11-28 16:45:51 +0100

    rewriting security config, trying new way

--- src/main/java/com/example/springrestapi/config/SecurityConfig.java
+++ src/main/java/com/example/springrestapi/config/SecurityConfig.java
@@ -1,50 +1,38 @@
 package com.example.springrestapi.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        JwtAuthenticationConverter jwtAuthenticationConverter = jwtAuthenticationConverter();
-
         http
-                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF using the new API
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/api/categories/**").hasRole("ADMIN")
-                                .requestMatchers("/api/places/public/**").permitAll()
-                                .requestMatchers("/api/places/**").authenticated()
-                                .requestMatchers(HttpMethod.DELETE, "/api/places/**").hasRole("ADMIN")  // Ensure DELETE requires ADMIN role
-                                .anyRequest().permitAll()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers(GET, "/api/categories/**").permitAll()
+                        .requestMatchers(POST, "/api/categories/**").hasAuthority("SCOPE_admin")
+                        .requestMatchers(GET, "/api/places/user/**").hasAuthority("SCOPE_user")
+                        .requestMatchers(GET, "/api/places/**").permitAll()
+                        .requestMatchers(GET, "/api/places/category/**").permitAll()
+                        .requestMatchers(GET, "/api/places/radius").permitAll()
+                        .requestMatchers(POST, "/api/places/**").hasAuthority("SCOPE_user")
+
+                        .anyRequest().authenticated()
                 )
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwt ->
-                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
-                        )
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
                 );
         return http.build();
-    }
-
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return jwtAuthenticationConverter;
     }
 }
